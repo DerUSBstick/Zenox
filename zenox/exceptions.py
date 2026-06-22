33bda7ab7107bd21a03c86ceb5685d15b7d058f9
DerUSBstick <74152790+DerUSBstick@users.noreply.github.com>
2026-06-22 18:35:26 +0200

    feat: add new exception classes for EnkaAPI, HoyolabAPI, and HoyolabLogin errors

--- zenox/exceptions.py
+++ zenox/exceptions.py
@@ -10,3 +10,24 @@ class ZenoxException(Exception):
 class InvalidInputError(ZenoxException):
     def __init__(self, message: LocaleStr):
         super().__init__(title=LocaleStr(key="invalid_input.title"), message=message)
+
+
+class EnkaAPIError(ZenoxException):
+    def __init__(self, status_code: int | None = None) -> None:
+        desc = LocaleStr(key="enka_api_error.description", status_code=status_code or "?")
+        super().__init__(title=LocaleStr(key="enka_api_error.title"), message=desc)
+        self.status_code = status_code
+
+
+class HoyolabAPIError(ZenoxException):
+    def __init__(self, retcode: int | None = None) -> None:
+        desc = LocaleStr(key="hoyolab_api_error.description", retcode=retcode or "?")
+        super().__init__(title=LocaleStr(key="hoyolab_api_error.title"), message=desc)
+        self.retcode = retcode
+
+
+class HoyolabLoginError(ZenoxException):
+    def __init__(self, retcode: int | None = None) -> None:
+        desc = LocaleStr(key="hoyolab_login_error.description", retcode=retcode or "?")
+        super().__init__(title=LocaleStr(key="hoyolab_login_error.title"), message=desc)
+        self.retcode = retcode
